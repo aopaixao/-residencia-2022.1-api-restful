9596688796e961fa9ede8506e877aef428570018
aopaixao <aopaixao@outlook.com>
2022-05-16 12:54:10 -0300

    Create livraria_v1.sql

---
+++ livraria_v1.sql
@@ -0,0 +1,14 @@
+CREATE TABLE editora (
+	editora_id serial NOT NULL,
+	editora_nome varchar(255) NULL,
+	PRIMARY KEY (editora_id)
+);
+
+
+CREATE TABLE livro (
+	livro_id serial NOT NULL,
+	livro_nome varchar(255) NULL,
+	editora_id int8,
+	PRIMARY KEY (livro_id),
+	FOREIGN KEY (editora_id) REFERENCES editora(editora_id)
+);
