4e59de0c2e101c470d171d713c5f3c09e99f468d
aopaixao <aopaixao@outlook.com>
2022-05-16 10:59:47 -0300

    Add files via upload

---
+++ src/main/java/com/residencia/firstapi/entity/Editora.java
@@ -0,0 +1,51 @@
+package com.residencia.firstapi.entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "editora")
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "editoraId")
+public class Editora {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "editora_id")
+    private Integer editoraId;
+
+    @Column(name = "editora_nome")
+    private String editoraNome;
+
+    @OneToMany(mappedBy = "editora")
+    //@JsonManagedReference
+    private List<Livro> livroList;
+
+    public Integer getEditoraId() {
+        return editoraId;
+    }
+
+    public void setEditoraId(Integer editoraId) {
+        this.editoraId = editoraId;
+    }
+
+    public String getEditoraNome() {
+        return editoraNome;
+    }
+
+    public void setEditoraNome(String editoraNome) {
+        this.editoraNome = editoraNome;
+    }
+
+    public List<Livro> getLivroList() {
+        return livroList;
+    }
+
+    public void setLivroList(List<Livro> livroList) {
+        this.livroList = livroList;
+    }
+}
---
+++ src/main/java/com/residencia/firstapi/entity/Livro.java
@@ -0,0 +1,51 @@
+package com.residencia.firstapi.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "livro")
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "livroId")
+public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "livro_id")
+    private Integer livroId;
+
+    @Column(name = "livro_nome")
+    private String livroNome;
+
+    @ManyToOne
+    @JoinColumn(name = "editora_id", referencedColumnName = "editora_id")
+    //@JsonBackReference
+    private Editora editora;
+
+    public Integer getLivroId() {
+        return livroId;
+    }
+
+    public void setLivroId(Integer livroId) {
+        this.livroId = livroId;
+    }
+
+    public String getLivroNome() {
+        return livroNome;
+    }
+
+    public void setLivroNome(String livroNome) {
+        this.livroNome = livroNome;
+    }
+
+    public Editora getEditora() {
+        return editora;
+    }
+
+    public void setEditora(Editora editora) {
+        this.editora = editora;
+    }
+}
